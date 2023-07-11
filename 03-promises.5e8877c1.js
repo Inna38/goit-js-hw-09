0e559ae235f2029f99c032c66cf8dc2d282b3efa
Inna38 <110390775+Inna38@users.noreply.github.com>
2023-07-11 14:21:26 +0000

    Deploying to gh-pages from @ Inna38/goit-js-hw-09@1562bc14eb7da3b508a6eca95be4004466fb5b83 🚀

--- 03-promises.5e8877c1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector('button[type="submit"]'),t=document.querySelector(".form");let n=null;t.addEventListener("input",o),e.addEventListener("submit",o);new Promise(((e,l)=>{n=setInterval((()=>{o()}),1e3),t[2].value&&clearInterval(n)}));function o(e,n){console.log("qqq"),console.dir(t[2].value)}
-//# sourceMappingURL=03-promises.5e8877c1.js.map
